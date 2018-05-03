fe4df289139684dc7a3ee7ecceaac6ebb80793cd
Víctor González <victor.gonzalez@geomati.co>
2018-05-03 09:51:28 +0200

    Implement Style

--- geoserver/Style.py
+++ geoserver/Style.py
@@ -1,12 +1,53 @@
+# pylint: disable=W0212
+
+"""
+Style
+"""
 from geoserver.Resource import Resource
 
 
 class Style(Resource):
+    """
+    An object representing a GeoServer style.
+
+    :param name: The name of the style
+    :param geoserver: The :class:`geoserver.GeoServer` instance this workspace belongs to.
+    :type name: string
+    :type geoserver: :class:`geoserver.GeoServer`
+    """
     def __init__(self, name, geoserver):
         Resource.__init__(self, name, geoserver)
 
     def delete(self):
+        """
+        Deletes the style from GeoServer.
+
+        :rtype: None
+        :raise: :class:`IOError` if any error occurs while requesting the REST API.
+        """
         self.geoserver._request('styles/' + self.name, method='DELETE')
 
     def set_sld(self, sld):
-        pass
+        """
+        Sets the SLD content for this style.
+
+        :param sld: SLD content to set.
+        :type sld: string
+        :rtype: None
+        :raise: :class:`IOError` if any error occurs while requesting the REST API.
+        """
+        self.geoserver._request(
+            'styles/' + self.name, method='PUT', extension='', data=sld,
+            headers={'Content-type': 'application/vnd.ogc.sld+xml'})
+
+    def get_sld(self):
+        """
+        Gets the SLD content for this style.
+
+        :return: the SLD content.
+        :rtype: string
+        """
+        style = self.geoserver._get('styles/' + self.name)
+        return self.geoserver._request(
+            'resource/styles/' + style['style']['filename'],
+            extension='')
--- test/test_Style.py
+++ test/test_Style.py
@@ -1,19 +1,37 @@
+# pylint: disable=too-many-public-methods,missing-docstring,I1101
+
 import unittest
-from geoserver.Style import Style
+from test.AbstractGeoServerTestCase import AbstractGeoServerTestCase
+import xml.etree.ElementTree as ET
 
 
-class StyleTestCase(unittest.TestCase):
-    def test_get_geoserver(self):
-        pass
+def _get_style_name(style):
+    elem = ET.fromstring(style.get_sld())
+    xpath = './/{http://www.opengis.net/sld}UserStyle/{http://www.opengis.net/sld}Name'
+    return elem.find(xpath).text
 
-    def test_delete(self):
-        pass
 
+class StyleTestCase(AbstractGeoServerTestCase):
     def test_set_sld(self):
-        pass
+        with open('test/burg.sld') as f:
+            burg = f.read()
+        with open('test/capitals.sld') as f:
+            capitals = f.read()
+
+        style = self.gs.get_style('burg')
+        self.assertEqual('burg', _get_style_name(style))
+        style.set_sld(capitals)
+        self.assertEqual('capitals', _get_style_name(style))
+        style.set_sld(burg)
 
     def test_set_sld_invalid(self):
-        pass
+        style = self.gs.get_style('burg')
+        try:
+            with open('test/invalid.sld') as f:
+                style.set_sld(f.read())
+                assert False
+        except IOError:
+            pass
 
 
 if __name__ == '__main__':
